ac5d9baca71317f0d3fee721c3f8dda98380f284
42 <42@koishi.chat>
2024-03-17 16:33:03 +0800

    v2.3.0

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ export function apply(ctx: Context, config: Config) {
         const { concept_scores } = await ctx.http.post('http://api.t4wefan.pub:51317/check_safety', data)
           .catch((e) => { logger.error(e) }) as ReviewResult // 草 写的好丑
         if (!concept_scores) return h.image(attrs.url)
-        const unsafe = concept_scores.some((score, i) => score + config.offset > config.threshold.offset[i])
+        const unsafe = concept_scores.some((score, i) => score + config.offset > config.threshold[i])
         if (config.debug) logger.info(`Got an image with scores: \n${concept_scores.join('\n')}`)
         if (!unsafe) return h.image(attrs.url)
         return h.i18n('rr-image-censor.detected_unsafe_images')
@@ -50,23 +50,16 @@ export function apply(ctx: Context, config: Config) {
   })
 }
 
-export const Config: Schema<Config> = Schema.intersect([
-  Schema.object({
-    debug: Schema.boolean().description('调试模式，打印每张图的评分到日志。').default(false),
-    offset: Schema.number().description('审核强度整体偏移量。').default(-0.016).max(1.0).min(-1.0),
-  }),
-  Schema.object({
-    threshold: Schema.object({
-      offset: Schema.array(Schema.number()).default(Array(17).fill(0))
-    }).collapse().experimental().description('每个分类的阈值微调。'),
-  })
-])
-
+export const Config: Schema<Config> = Schema.object({
+  debug: Schema.boolean().description('调试模式，打印每张图的评分到日志。').default(false),
+  offset: Schema.number().description('审核强度整体偏移量。').default(-0.016).max(1.0).min(-1.0),
+  threshold: Schema.array(Schema.number()).default(Array(17).fill(0)).collapse().experimental().description('每个分类的阈值微调。')
+})
 
 export interface Config {
   debug?: boolean
   offset?: number
-  threshold?: { offset: number[] }
+  threshold?: number[]
 }
 export interface NsfwCheck {
   image: string
